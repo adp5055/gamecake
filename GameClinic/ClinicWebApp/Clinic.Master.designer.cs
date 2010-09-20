d978aceacd350c3b0e2217a03a9ab162e241cd2f
hui.li627 <hui.li627@a42524db-bf45-01dd-ffc7-5aa353b232d5>
2010-09-20 22:58:19 +0000

    Created our own CSS and updated the website to use them.

--- GameClinic/ClinicWebApp/Clinic.Master.designer.cs
+++ GameClinic/ClinicWebApp/Clinic.Master.designer.cs
@@ -64,7 +64,7 @@ namespace ClinicWebApp {
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.UserControl NavBar;
+        protected global::ClinicWebApp.Controls.NavBar NavBar;
         
         /// <summary>
         /// ContentPlaceHolder1 control.
