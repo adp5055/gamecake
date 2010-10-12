52ed78702e2507d4d859b21a9b88ab3d82638ab9
hui.li627 <hui.li627@a42524db-bf45-01dd-ffc7-5aa353b232d5>
2010-10-12 23:01:55 +0000

    Included Register.aspx and other minor fixes.

---
+++ trunk/GameClinic/ClinicWebApp/App_Code/Entities/Account.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ClinicWebApp.App_Code.Entities
+{
+    public class Account
+    {
+        private int _account_id, _phone_number, _zip;
+        private string _gender, _first_name, _last_name, _email_address, _street_address, _state, _city;
+        private DateTime _dateofbirth;
+
+        public int Zip
+        {
+            get { return _zip; }
+            set { _zip = value; }
+        }
+
+        public int Phone_number
+        {
+            get { return _phone_number; }
+            set { _phone_number = value; }
+        }
+        
+        public string City
+        {
+            get { return _city; }
+            set { _city = value; }
+        }
+
+        public string State
+        {
+            get { return _state; }
+            set { _state = value; }
+        }
+
+        public string Street_address
+        {
+            get { return _street_address; }
+            set { _street_address = value; }
+        }
+        
+        public int Account_id
+        {
+            get { return _account_id; }
+            set { _account_id = value; }
+        }
+
+        public string Email_address
+        {
+            get { return _email_address; }
+            set { _email_address = value; }
+        }
+
+        public string Last_name
+        {
+            get { return _last_name; }
+            set { _last_name = value; }
+        }
+
+        public string First_name
+        {
+            get { return _first_name; }
+            set { _first_name = value; }
+        }
+
+        public string Gender
+        {
+            get { return _gender; }
+            set { _gender = value; }
+        }
+
+        public DateTime Dateofbirth
+        {
+            get { return _dateofbirth; }
+            set { _dateofbirth = value; }
+        }
+    }
+}
---
+++ trunk/GameClinic/ClinicWebApp/Pages/Register.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ClinicWebApp.App_Code.Entities;
+
+namespace ClinicWebApp.Pages
+{
+    public partial class Register : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Submit_Click(object sender, EventArgs e)
+        {
+            App_Code.Entities.Account cust_account = new App_Code.Entities.Account();
+            if (radFemale.Checked)
+                cust_account.Gender = "Female";
+            else if (radMale.Checked)
+                cust_account.Gender = "Male";
+            cust_account.First_name = txtFirstName.Text;
+            cust_account.Last_name = txtLastName.Text;
+            cust_account.Dateofbirth = Convert.ToDateTime(txtDateOfBirth.Text);
+            cust_account.Email_address = txtEmailAddress.Text;
+            cust_account.City = txtCity.Text;
+            cust_account.State = txtState.Text;
+            cust_account.Street_address = txtStreetAddress.Text;
+            cust_account.Zip = Convert.ToInt32(txtZip.Text);
+            cust_account.Phone_number = Convert.ToInt32(txtPhone.Text);
+
+        }
+    }
+}
