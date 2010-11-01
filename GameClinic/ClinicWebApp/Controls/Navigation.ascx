bb92316fb3281054fea01cb86b543c9b99a844ef
hiestand.53@gmail.com <hiestand.53@gmail.com@a42524db-bf45-01dd-ffc7-5aa353b232d5>
2010-11-01 03:54:09 +0000

    navigation and breadcrumbs

--- GameClinic/ClinicWebApp/Controls/Navigation.ascx
+++ GameClinic/ClinicWebApp/Controls/Navigation.ascx
@@ -16,7 +16,14 @@
         width: 139px;
         height: 170px;
     }
+    .list
+    {
+    	font-size: small;
+    }
 </style>
+
+&nbsp;
+<asp:Label ID="Label1" runat="server" Font-Bold="True" Text="Quick Navigation"></asp:Label>
 
 <p>
     <asp:Image id="picMario" alt="Mario" class="style1" src="../Images/mario-big.jpg" runat = "server" /></p>
@@ -24,6 +31,65 @@
     <asp:Image id="picSonic" alt="Sonic" class="style2" src="../Images/sonic-big.jpg" runat = "server" /></p>
 <p>
     <asp:Image id="picZelda" alt="Zelda" class="style3" src="../Images/zelda-pic.jpg" runat = "server" /></p>
-<p>
-    &nbsp;</p>
+<p class = "list">
+    <asp:Label ID="Genres" runat="server" Font-Size="Small" Text="-GENRES"></asp:Label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypFPS" runat="server">First Person Shooter</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypRPS" runat="server">Role Playing</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypShooter" runat="server">Shooter</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypSports" runat="server">Sports</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypChild" runat="server">Children</asp:HyperLink><br />
+</p>
+<p class = "list">
+    <asp:Label ID="Console" runat="server" Font-Size="Small" Text="-CONSOLES"></asp:Label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypPS2" runat="server">Playstation 2</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypPS3" runat="server">Playstation 3</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypXbox" runat="server">X-Box</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypXbox360" runat="server">X-Box</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypPC" runat="server">PC</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypN64" runat="server">N-64</asp:HyperLink><br />
+</p>
+<p class = "list">
+    <asp:Label ID="Price" runat="server" Font-Size="Small" Text="-PRICE"></asp:Label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hyp010" runat="server">$  0-10</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hyp1120" runat="server">$  11-20</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hyp2130" runat="server">$  21-30</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hyp3140" runat="server">$  31-40</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hyp50" runat="server">$  50+</asp:HyperLink><br />
+</p>
+<p class = "list">
+    <asp:Label ID="Ratings" runat="server" Font-Size="Small" Text="-RATINGS"></asp:Label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypEvry" runat="server">Everyone</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypEvry10" runat="server">Everyone 10+</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypTeen" runat="server">Teen</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypMat" runat="server">Mature</asp:HyperLink><br />
+</p>
+<p class = "list">
+    <asp:Label ID="PlayerCount" runat="server" Font-Size="Small" Text="-PLAYER COUNT"></asp:Label><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypSingle" runat="server">Single Player</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypOnline" runat="server">Online Play</asp:HyperLink><br />
+    &nbsp;&nbsp;&nbsp;&nbsp;
+    <asp:HyperLink ID="hypMulti" runat="server">Multiplayer</asp:HyperLink><br />
+</p>
 
--- GameClinic/ClinicWebApp/Controls/Navigation.ascx.designer.cs
+++ GameClinic/ClinicWebApp/Controls/Navigation.ascx.designer.cs
@@ -13,6 +13,15 @@ namespace ClinicWebApp.Controls {
     public partial class Navigation {
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
         /// picMario control.
         /// </summary>
         /// <remarks>
@@ -38,5 +47,257 @@ namespace ClinicWebApp.Controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Image picZelda;
+        
+        /// <summary>
+        /// Genres control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Genres;
+        
+        /// <summary>
+        /// hypFPS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypFPS;
+        
+        /// <summary>
+        /// hypRPS control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypRPS;
+        
+        /// <summary>
+        /// hypShooter control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypShooter;
+        
+        /// <summary>
+        /// hypSports control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypSports;
+        
+        /// <summary>
+        /// hypChild control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypChild;
+        
+        /// <summary>
+        /// Console control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Console;
+        
+        /// <summary>
+        /// hypPS2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypPS2;
+        
+        /// <summary>
+        /// hypPS3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypPS3;
+        
+        /// <summary>
+        /// hypXbox control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypXbox;
+        
+        /// <summary>
+        /// hypXbox360 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypXbox360;
+        
+        /// <summary>
+        /// hypPC control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypPC;
+        
+        /// <summary>
+        /// hypN64 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypN64;
+        
+        /// <summary>
+        /// Price control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Price;
+        
+        /// <summary>
+        /// hyp010 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyp010;
+        
+        /// <summary>
+        /// hyp1120 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyp1120;
+        
+        /// <summary>
+        /// hyp2130 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyp2130;
+        
+        /// <summary>
+        /// hyp3140 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyp3140;
+        
+        /// <summary>
+        /// hyp50 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hyp50;
+        
+        /// <summary>
+        /// Ratings control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Ratings;
+        
+        /// <summary>
+        /// hypEvry control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypEvry;
+        
+        /// <summary>
+        /// hypEvry10 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypEvry10;
+        
+        /// <summary>
+        /// hypTeen control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypTeen;
+        
+        /// <summary>
+        /// hypMat control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypMat;
+        
+        /// <summary>
+        /// PlayerCount control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label PlayerCount;
+        
+        /// <summary>
+        /// hypSingle control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypSingle;
+        
+        /// <summary>
+        /// hypOnline control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypOnline;
+        
+        /// <summary>
+        /// hypMulti control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink hypMulti;
     }
 }
